7f5cb24c7dff324df5759dd2dfbbb32bf3b4100c
Harshita <chandaliyaharshita222@gmail.com>
2023-05-09 19:01:40 +0530

    Added Welcome Message

---
+++ Basic_Practice_Problems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basic_Practice_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Practice Problems!");
+        }
+    }
+}
