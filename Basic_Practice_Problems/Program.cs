4739abad4f48fec443b20186711f60beee63cd36
Harshita <chandaliyaharshita222@gmail.com>
2023-05-15 20:14:27 +0530

    Program to print a pattern

--- Basic_Practice_Problems/Program.cs
+++ Basic_Practice_Problems/Program.cs
@@ -7,36 +7,16 @@ namespace Basic_Practice_Problems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Practice Problems!");
-            Console.WriteLine("Find Unique elements in an array");
-            int[] arr = new int[100];
-            int i, j, n;
-            Console.Write("Enter array size: ");
+            Console.WriteLine("Program to Print a Pattern");
+            int i, n;
+            Console.Write("Enter number of rows to draw pattern: ");
             n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter {0} elements in the array", n);
 
             for (i = 0; i < n; i++)
             {
-                Console.Write("element - {0} : ", i);
-                arr[i] = Convert.ToInt32(Console.ReadLine());
-            }
-
-            Console.WriteLine("Unique elements in array:");
-            
-            for (i = 0; i < n; i++)
-            {
-                bool isUnique = false;
-                for (j = 0; j < i; j++)
-                {
-                    if (arr[i] == arr[j])
-                    {
-                        isUnique = true;
-                        break;
-                    }
-                }
-                if (!isUnique)
-                {
-                    Console.WriteLine(arr[i]);
-                }
+                string pattern = new string('*', (n - i));
+                string res = string.Join(" ", pattern.ToArray());
+                Console.WriteLine(res);
             }
         }
     }
