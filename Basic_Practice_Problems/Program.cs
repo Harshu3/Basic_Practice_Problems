4d3b6264cd24119a1aeaabfd9d8bdc7a01c07cbb
Harshita <chandaliyaharshita222@gmail.com>
2023-05-22 19:55:51 +0530

    Reverse Each Word in a given String

--- Basic_Practice_Problems/Program.cs
+++ Basic_Practice_Problems/Program.cs
@@ -7,26 +7,17 @@ namespace Basic_Practice_Problems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Practice Problems!");
-            Console.WriteLine("Program to Print a Pattern");
-            int size, row, column;
-            Console.Write("Enter size to draw pattern: ");
-            size = Convert.ToInt32(Console.ReadLine());
-
-            for ( row = 0; row < size; row++)
+            Console.WriteLine("Program to Reverse Each Word in a given String!");
+            Console.WriteLine("Enter a String:");
+            string s = Console.ReadLine();
+            string rev = "";
+            int length = s.Length - 1;
+            while (length >= 0)
             {
-                for ( column = 0; column < size; column++)
-                {
-                    if (row == 0 || column == 0 || row == size - 1 || column == size - 1)
-                    {
-                        Console.Write("* ");
-                    }
-                    else
-                    {
-                        Console.Write("  ");
-                    }
-                }
-                Console.WriteLine();
+                rev = rev + s[length];
+                length--;
             }
+            Console.WriteLine("After reverse: " + rev);
         }
     }
 }
